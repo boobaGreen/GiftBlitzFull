b6775ae21ef52171b99c6587d56d6ca578b9c0e1
boobaGreen <devboobagreen@gmail.com>
2026-02-01 20:16:27 +0100

    Deploy to IOTA Testnet3

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { IotaClientProvider, WalletProvider } from '@iota/dapp-kit'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { networkConfig } from './config/iotaConfig.ts'
+import contracts from './data/contracts.json'
 import '@iota/dapp-kit/dist/index.css'
 import './index.css'
 import App from './App.tsx'
@@ -12,7 +13,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <IotaClientProvider networks={networkConfig} defaultNetwork="local">
+      <IotaClientProvider networks={networkConfig} defaultNetwork={(contracts.NETWORK as 'testnet' | 'local') || 'local'}>
         <WalletProvider autoConnect>
           <App />
         </WalletProvider>
